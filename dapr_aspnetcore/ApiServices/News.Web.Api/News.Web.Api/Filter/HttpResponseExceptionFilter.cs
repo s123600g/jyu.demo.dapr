8eaa6fdc593ce5092aeb498277892d7ad4e41587
JYUN-YU LI <s123600g@gmail.com>
2021-06-12 00:48:34 +0800

    feat: News Web Api 初步實作。
    1. 測試攔截Action Filter注入。
    2. 實作Activity news controller。

---
+++ dapr_aspnetcore/ApiServices/News.Web.Api/News.Web.Api/Filter/HttpResponseExceptionFilter.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace News.Web.Api.Filter
+{
+    /// <summary>
+    /// 過濾Api Action執行錯誤狀態
+    /// </summary>
+    public class HttpResponseExceptionFilter : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context) { }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            // 偵測當前Http Context是否有發生例外錯誤，如果有的話就要接手處理回應訊息
+            if (context.Exception != null)
+            {
+                Exception getException = context.Exception;
+
+                context.Result = new ObjectResult(getException.GetType().Name)
+                {
+                    StatusCode = StatusCodes.Status500InternalServerError
+                };
+                context.ExceptionHandled = true;
+            }
+        }
+    }
+}
