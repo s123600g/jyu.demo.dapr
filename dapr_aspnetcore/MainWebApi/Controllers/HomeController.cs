5a0673a6ef48c1df68a4f6042373c0e4eb1b2c43
李俊諭 JYUN-YU LI <s123600g@gmail.com>
2021-06-27 12:28:55 +0800

    feat: Update project arch and remove dapr_aspnetcore api.

---
+++ dapr_aspnetcore/MainWebApi/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MainWebApi.Controllers
+{
+    [Area("Home")]
+    [Route("[area]/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public HomeController() { }
+
+        [HttpGet("Index")]
+        public ActionResult Index()
+        {
+            return Ok("The Api is Running.");
+        }
+    }
+}
